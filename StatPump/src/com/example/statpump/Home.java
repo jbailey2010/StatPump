6f79eb12ffda3d7b11e2feda14e1293fb4a10339
Jeff Bailey <jbailey2010@gmail.com>
2013-05-29 22:42:53 -0400

    Initial layout done

--- StatPump/src/com/example/statpump/Home.java
+++ StatPump/src/com/example/statpump/Home.java
@@ -1,8 +1,13 @@
 package com.example.statpump;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Home extends Activity {
 
@@ -10,6 +15,15 @@ public class Home extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+		ListView listview = (ListView)findViewById(R.id.listView1);
+		List<String> temp = new ArrayList<String>();
+		for(int i = 0; i < 59; i++)
+		{
+			temp.add(Integer.toString(i));
+		}
+	    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+	            android.R.layout.simple_list_item_1, temp);
+	    listview.setAdapter(adapter);
 	}
 
 	@Override
