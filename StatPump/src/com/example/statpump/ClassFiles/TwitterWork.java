5c36c140b466e87313cb73cc183793f7faf85150
Jeff Bailey <jbailey2010@gmail.com>
2013-06-07 21:47:02 -0400

    Gets PIN and redirect URL

--- StatPump/src/com/example/statpump/ClassFiles/TwitterWork.java
+++ StatPump/src/com/example/statpump/ClassFiles/TwitterWork.java
@@ -18,7 +18,16 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Does the twitter-related work
@@ -26,6 +35,11 @@ import android.os.AsyncTask;
 public class TwitterWork 
 {
 	String validURL = "";
+	int pin = -1;
+	/**
+	 * Calls the validation URL asynctask
+	 * @param cont
+	 */
 	public static void twitterInitial(Context cont)
 	{
 		TwitterWork obj = new TwitterWork();
@@ -33,6 +47,11 @@ public class TwitterWork
 	    task.execute(cont);
 	}
 	
+	/**
+	 * Gets the validation URL from twitter
+	 * @author Jeff
+	 *
+	 */
 	public class TwitterConnection extends AsyncTask<Object, Void, Void> 
 	{
 		ProgressDialog pdia;
@@ -55,11 +74,13 @@ public class TwitterWork
 		protected void onPostExecute(Void result){
 		   super.onPostExecute(result);
 		   pdia.dismiss();
+		   handleURL(act);
 		}
 		
 	    @Override
 	    protected Void doInBackground(Object... data) 
 	    {
+	    	final Context cont = (Context)data[0];
 	    	Twitter twitter = TwitterFactory.getSingleton();
 	        twitter.setOAuthConsumer("De64oQ246ojYaGQfVb1rw",
 	        		"xVpbhUMjPceJDD6pTU2qpjX4qvbBFi1eBW7vr3pg3YI");
@@ -68,12 +89,101 @@ public class TwitterWork
 		        AccessToken accessToken = null;
 		        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		        validURL = requestToken.getAuthorizationURL();
-		        System.out.println(validURL);
 			} catch (TwitterException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 	        return null;
 	    }
-	  }
+	}
+	
+	/**
+	 * Creates a dialog to get the user to validate it, then enter the pin
+	 * @param cont
+	 */
+	public void handleURL(final Activity cont)
+	{
+		final Dialog dialog = new Dialog(cont, R.style.RoundCornersFull);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.twitter_login);
+		WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+	    lp.copyFrom(dialog.getWindow().getAttributes());
+	    lp.width = WindowManager.LayoutParams.FILL_PARENT;
+	    dialog.getWindow().setAttributes(lp);
+	    dialog.show();
+	    Button validate = (Button)dialog.findViewById(R.id.twitter_confirm_go);
+	    validate.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(Intent.ACTION_VIEW);
+				i.setData(Uri.parse(validURL));
+				cont.startActivity(i);
+				dialog.dismiss();
+				handlePin(cont);
+			}
+	    });
+	    Button cancel = (Button)dialog.findViewById(R.id.twitter_confirm_cancel);
+	    cancel.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				dialog.dismiss();
+				return;
+			}
+	    });
+	}
+	
+	/**
+	 * Gets and fetches the valid pin
+	 * @param cont
+	 */
+	public void handlePin(final Activity cont)
+	{
+		final Dialog dialog = new Dialog(cont, R.style.RoundCornersFull);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.twitter_validate_pin);
+		WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+	    lp.copyFrom(dialog.getWindow().getAttributes());
+	    lp.width = WindowManager.LayoutParams.FILL_PARENT;
+	    dialog.getWindow().setAttributes(lp);
+	    dialog.show();	
+	    final EditText input = (EditText)dialog.findViewById(R.id.twitter_pin_field);
+	    Button submit = (Button)dialog.findViewById(R.id.twitter_pin_go);
+	    submit.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				String text = input.getText().toString();
+				if(text.length() < 5)
+				{
+					Toast.makeText(cont, "Please Enter a Valid PIN", Toast.LENGTH_SHORT).show();
+				}
+				else
+				{
+					try{
+						pin = Integer.parseInt(text);
+						dialog.dismiss();
+						finalizeValidation(cont);
+					}
+					catch (NumberFormatException e){
+						Toast.makeText(cont, "Please Enter a PIN of Only Numbers", Toast.LENGTH_SHORT).show();
+					}
+				}
+			}
+	    });
+	    Button cancel = (Button)dialog.findViewById(R.id.twitter_pin_cancel);
+	    cancel.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				dialog.dismiss();
+			}
+	    });
+	}
+	
+	/**
+	 * 
+	 * @param cont
+	 */
+	public void finalizeValidation(Context cont)
+	{
+		
+	}
 }
