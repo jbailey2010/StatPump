f44bc176f98ae39c734cf25d351f097f2726266a
Jeff Bailey <jbailey2010@gmail.com>
2013-06-27 00:24:42 -0400

    All but soccer done

--- StatPump/src/com/example/statpump/InterfaceAugmentation/ManageInput.java
+++ StatPump/src/com/example/statpump/InterfaceAugmentation/ManageInput.java
@@ -75,6 +75,13 @@ public class ManageInput
 		else if(selection.equals("Australian Football"))
 		{
 			image.setImageResource(R.drawable.ausfootball);
+			sports.add("AFL - Australia");
+			sports.add("SANFL - Australia");
+			sports.add("Foxtel Cup - Australia");
+			sports.add("DAFL - Denmark");
+			sports.add("AFLG - Germany");
+			sports.add("SAFF Premier League - Sweden");
+			sports.add("International Cup - World");
 		}
 		else if(selection.equals("Baseball"))
 		{
@@ -87,10 +94,52 @@ public class ManageInput
 		else if(selection.equals("Cricket"))
 		{
 			image.setImageResource(R.drawable.cricket);
+			sports.add("Asia Cup - Asia");
+			sports.add("ACC Trophy Elite - Asia");
+			sports.add("Asian Test Championship - Asia");
+			sports.add("KFC Twenty20 Big Bash - Australia");
+			sports.add("Ryobi One-Day Cup - Australia");
+			sports.add("Scheffield Shield - Australia");
+			sports.add("NCL - Bangladesh");
+			sports.add("NCL One-Day - Bangladesh");
+			sports.add("BPL T20 - Bangladesh");
+			sports.add("County Championship 1 - England");
+			sports.add("County Championship 2 - England");
+			sports.add("Yorkshire Bank 40 - England");
+			sports.add("Friends Life t20 - England");
+			sports.add("India Premier League - India");
+			sports.add("Carribean Twenty20 - N/C America");
+			sports.add("Ford Trophy - New Zealand");
+			sports.add("HRV Cup - New Zealand");
+			sports.add("Plunket Shield - New Zealand");
+			sports.add("Sunfoil Series - South Africa");
+			sports.add("Momentum 1-Day Cup - South Africa");
+			sports.add("T20 Series - South Africa");
+			sports.add("Premier League - Sri Lanka");
+			sports.add("Champions League Twenty20 - World");
+			sports.add("ICC World Cup - World");
+			sports.add("ICC World Twenty20 - World");
+			sports.add("ICC World Twenty20 Qualifiers - World");
+			sports.add("ICC Test Championship - World");
+			sports.add("ICC ODI Championship - World");
+			sports.add("Twenty20 Internationals - World");
+			sports.add("ICC Champions Trophy - World");
+			sports.add("ICC Intercontinental Cup - World");
+			sports.add("ICC WCL Championship - World");
+			sports.add("ICC World Cricket League - World");
+			sports.add("ICC U19 World Cup - World");
 		}
 		else if(selection.equals("Golf"))
 		{
 			image.setImageResource(R.drawable.golf);
+			sports.add("PGA Tour");
+			sports.add("European Tour");
+			sports.add("LPGA Tour");
+			sports.add("Asian Tour");
+			sports.add("Japan Golf Tour");
+			sports.add("Sunshine Tours");
+			sports.add("Web.com Tour");
+			sports.add("Champions Tour");
 		}
 		else if(selection.equals("Handball"))
 		{
@@ -99,6 +148,72 @@ public class ManageInput
 		else if(selection.equals("Hockey"))
 		{
 			image.setImageResource(R.drawable.hockey);
+			sports.add("ALH - Asia");
+			sports.add("AIHL - Australia");
+			sports.add("EHL - Austria");
+			sports.add("Nationalliga - Austria");
+			sports.add("Extraliga - Belarus");
+			sports.add("Elite League - Belgium");
+			sports.add("Memorial Cup - Canada");
+			sports.add("OHL - Canada");
+			sports.add("WHL - Canada");
+			sports.add("LHJMQ - Canada");
+			sports.add("Extraliga - Czech Republic");
+			sports.add("1. Liga - Czech Republic");
+			sports.add("Superisligaen - Denmark");
+			sports.add("Elite League - England");
+			sports.add("Meistriliiga - Estonia");
+			sports.add("Continental Cup - Europe");
+			sports.add("Euro Ice Hockey Challenge - Europe");
+			sports.add("European Trophy - Europe");
+			sports.add("Euro Hockey Tour - Europe");
+			sports.add("Inter-National-League - Europe");
+			sports.add("SM-Liiga - Finland");
+			sports.add("Mestis - Finland");
+			sports.add("Lique Magnus - France");
+			sports.add("DEL - Germany");
+			sports.add("2. Bundesliga - Germany");
+			sports.add("Interliga - Hungary");
+			sports.add("Serie A1 - Italy");
+			sports.add("LHL - Latvia");
+			sports.add("Eredivisie - Netherlands");
+			sports.add("Bekercompetitie - Netherlands");
+			sports.add("GET-ligaen - Norway");
+			sports.add("PLH - Poland");
+			sports.add("Campionatul National - Romania");
+			sports.add("KHL - Russia");
+			sports.add("VHL - Russia");
+			sports.add("MHL - Russia");
+			sports.add("Extraliga - Slovakia");
+			sports.add("Slohokej Liga - Slovenia");
+			sports.add("Liga Nacional - Spain");
+			sports.add("Elitserien - Sweden");
+			sports.add("HockeyAllsvenskan - Sweden");
+			sports.add("Division 1 - Sweden");
+			sports.add("Nationalliga A - Switzerland");
+			sports.add("Nationalliga B - Switzerland");
+			sports.add("PHL - Ukraine");
+			sports.add("NHL - United States");
+			sports.add("AHL - United States");
+			sports.add("ECHL - United States");
+			sports.add("CHL - United States");
+			sports.add("FHL - United States");
+			sports.add("SPHL - United States");
+			sports.add("Olympic Qualification - World");
+			sports.add("Olympic Qualification (W) - World");
+			sports.add("Olympics");
+			sports.add("Olympics (W)");
+			sports.add("World Championship");
+			sports.add("World Championship div I - World");
+			sports.add("World Championship div II - World");
+			sports.add("World Championship div III - World");
+			sports.add("World Championship U20 - World");
+			sports.add("World Championship U18 - World");
+			sports.add("World Championship (W) - World");
+			sports.add("World Championship div I (W) - World");
+			sports.add("World Championship div II (W) - World");
+			sports.add("World Championship div III (W) - World");
+			
 		}
 		else if(selection.equals("Motorsports"))
 		{
@@ -141,6 +256,71 @@ public class ManageInput
 		else if(selection.equals("Rugby"))
 		{
 			image.setImageResource(R.drawable.rugby);
+			sports.add("National Rugby League - Australia");
+			sports.add("Ron Massey Cup - Australia");
+			sports.add("State of Origin - Australia");
+			sports.add("Division 1 - Belgium");
+			sports.add("Canadian Rugby Championship - Canada");
+			sports.add("Extraliga - Czech Republic");
+			sports.add("Aviva Premiership - England");
+			sports.add("Super League - England");
+			sports.add("Championship - England");
+			sports.add("Championship 1 - England");
+			sports.add("Northern Rail Cup - England");
+			sports.add("Premiership 7s - England");
+			sports.add("Aviva A League - England");
+			sports.add("NCL - England");
+			sports.add("RaboDirect PRO12 - Europe");
+			sports.add("European Rugby Cup - Europe");
+			sports.add("Six Nations - Europe");
+			sports.add("European Nations Cup - Europe");
+			sports.add("U20 European Championship - Europe");
+			sports.add("U19 European Championship - Europe");
+			sports.add("U18 European Championship - Europe");
+			sports.add("U18 European Championship Div A - Europe");
+			sports.add("U18 European Championship Div B - Europe");
+			sports.add("U18 European Championship Div C - Europe");
+			sports.add("U18 European Championship Div D - Europe");
+			sports.add("Regional Rugby Championship - Europe");
+			sports.add("North Sea Cup - Europe");
+			sports.add("British & Irish Cup - Europe");
+			sports.add("Viking Tri-Nations - Europe");
+			sports.add("Anglo Welsh Cup - Europe");
+			sports.add("Challenge Cup - Europe");
+			sports.add("7's Grand Prix Series - Europe");
+			sports.add("7's Division A - Europe");
+			sports.add("Top 14 - France");
+			sports.add("Pro D2 - France");
+			sports.add("Bundesliga - Germany");
+			sports.add("Super 12 - Italy");
+			sports.add("Ereklasse - Netherlands");
+			sports.add("1e Klasse - Netherlands");
+			sports.add("ITM Cup - New Zealand");
+			sports.add("Pacific Nations Cup - Oceania");
+			sports.add("Superliga - Romania");
+			sports.add("RBS Premier League - Scotland");
+			sports.add("RBS National League - Scotland");
+			sports.add("Currie Cup - South Africa");
+			sports.add("Vodacom Cup - South Africa");
+			sports.add("South American Rugby Championship - South America");
+			sports.add("Division de Honor - Spain");
+			sports.add("Division de Honor B - Spain");
+			sports.add("Allsvenskan - Sweden");
+			sports.add("Premiership - Wales");
+			sports.add("World Cup - World");
+			sports.add("Four Nations - World");
+			sports.add("Rugby League World Cup - World");
+			sports.add("Super 15 - World");
+			sports.add("The Rugby Championship - World");
+			sports.add("IRB Junior World Championship - World");
+			sports.add("IRB Junior World Rugby Trophy - World");
+			sports.add("Churchill Cup - World");
+			sports.add("Test Series - World");
+			sports.add("World Club Challenge - World");
+			sports.add("RWC Sevens - World");
+			sports.add("IRB Sevens World Series - World");
+			sports.add("RLWC Qualifiers - World");
+			sports.add("Americas Rugby Championship - World");
 		}
 		else if(selection.equals("Tennis"))
 		{
@@ -197,12 +377,12 @@ public class ManageInput
 			sports.add("Olympics Qualification (W) - Europe");
 			sports.add("Miesten Liiga - Finland");
 			sports.add("Naisten Liiga - Finland");
-			sports.add("Lique A - France");
-			sports.add("Lique B - France");
+			sports.add("Ligue A - France");
+			sports.add("Ligue B - France");
 			sports.add("Nationale 1 - France");
 			sports.add("Coupe de France - France");
 			sports.add("Trophee des Champions - France");
-			sports.add("Lique A feminin - France");
+			sports.add("Ligue A feminin - France");
 			sports.add("Division Excellence - France");
 			sports.add("Nationale 1F - France");
 			sports.add("Coupe de France (W) - France");
@@ -263,8 +443,8 @@ public class ManageInput
 			sports.add("Elitserien damer - Sweden");
 			sports.add("Nationalliga A - Switzerland");
 			sports.add("Nationalliga A (W) - Switzerland");
-			sports.add("Erkekler 1. Liqi - Turkey");
-			sports.add("Bayanlar 1. Liqi - Turkey");
+			sports.add("Erkekler 1. Ligi - Turkey");
+			sports.add("Bayanlar 1. Ligi - Turkey");
 			sports.add("World League - World");
 			sports.add("FIVB World Grand Prix - World");
 			sports.add("Olympics - World");
