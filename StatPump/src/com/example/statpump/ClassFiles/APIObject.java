2269579f2f96d8f1ed12fb836227b4e6b7dee12b
Jeff Bailey <bailey27@illinois.edu>
2013-07-13 00:47:42 -0400

    Some more interaction work

--- StatPump/src/com/example/statpump/ClassFiles/APIObject.java
+++ StatPump/src/com/example/statpump/ClassFiles/APIObject.java
@@ -9,16 +9,21 @@ import java.util.Map;
  */
 public class APIObject 
 {
+	//Maps sport to sport id
 	public Map<String, Integer> sportIDMap = new HashMap<String, Integer>();
 	public String sport;
+	public int sportID;
+	//Team string and id
 	public String team1;
 	public int team1ID;
 	public String team2;
 	public int team2ID;
+	//Year ID for later queries
 	public int yearID;
-	public int sportID;
+	//The constant part of the url for the query
 	public String urlBase = "http://api.globalsportsmedia.com/";
 	public String urlValidate = "&authkey=865c0c0b4ab0e063e5caa3387c1a8741&username=statp";
+	
 	/**
 	 * Maps the sports to ids...etc. Basic set up stuff.
 	 */
@@ -38,5 +43,16 @@ public class APIObject
 		sportIDMap.put("Soccer - UEFA Champions League", 10);
 		sportIDMap.put("Soccer - Primera Division", 7);
 		sportIDMap.put("Soccer - MLS", 33);
+	}
+	
+	/**
+	 * Sets the sport selection, getting the relevant data
+	 * @param selection
+	 */
+	public void sportSelected(String selection)
+	{
+		String sport = selection.split(" \\(")[0];
+		this.sport = sport;
+		this.sportID = this.sportIDMap.get(this.sport);
 	}
 }
